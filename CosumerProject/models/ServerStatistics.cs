4e17522dacddb1321718f2997252adc7223dad7c
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-19 00:45:45 +0200

    add servert statistics model for the consumer

---
+++ CosumerProject/models/ServerStatistics.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumerProject.models
+{
+    public class ServerStatistics
+    {
+        public string ServerIdentifier { get; set; }
+        public double MemoryUsage { get; set; } // in MB
+        public double AvailableMemory { get; set; } // in MB
+        public double CpuUsage { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
