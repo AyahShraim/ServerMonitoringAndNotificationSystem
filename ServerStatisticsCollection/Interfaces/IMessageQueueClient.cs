6fbebf5a17406a2842014f705f2d765ff05ac560
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-19 00:29:31 +0200

    Add RabbitMq publishing service

---
+++ ServerStatisticsCollection/Interfaces/IMessageQueueClient.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+
+namespace ServerStatisticsCollection.Interfaces
+{
+    public interface IMessageQueueClientPublisher
+    {
+        IConnection CreateConnection();
+        Task PublishAsync<T>(string topic, T message);    
+    }
+}
