169e590a494fcd32e02af9c877a35fca9a3dc05c
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-19 00:13:54 +0200

    add config models

---
+++ ServerStatisticsCollection/Models/ConfigModels/RabbitMqConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerStatisticsCollection.Models.ConfigModels
+{
+    public record RabbitMqConfiguration
+    {
+        public string? HostName { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
