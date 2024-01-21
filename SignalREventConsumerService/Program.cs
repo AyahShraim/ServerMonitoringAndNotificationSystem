23a66779296130d53317ed8630b6f288e38b23e5
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-21 10:42:45 +0200

    setup & Call the consumer service

---
+++ SignalREventConsumerService/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using SignalREventConsumerService.Extensions;
+using SignalREventConsumerService.Services;
+
+var configuration = SignalRConfigHelper.GetConfiguration();
+
+string SignalRUrl = configuration.GetSection("SignalRConfig:SignalRUrl").Value;
+var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+ILogger logger = loggerFactory.CreateLogger("SignalRLogger");
+
+var signalRConsumer = new SignalREventConsumer(SignalRUrl, logger);
+await signalRConsumer.StartAsync();
+signalRConsumer.SubscribeToEvents();
+await Task.Delay(Timeout.Infinite);
+
+
