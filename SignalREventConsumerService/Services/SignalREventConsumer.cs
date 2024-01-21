38657cb2bf7c560140ffd18b9922afb39abc5f98
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-21 10:42:14 +0200

    Implement event consumer in SignalREventConsumerService project

---
+++ SignalREventConsumerService/Services/SignalREventConsumer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace SignalREventConsumerService.Services
+{
+    public class SignalREventConsumer : IDisposable
+    {
+        private readonly HubConnection _hubConnection;
+        private readonly ILogger _logger;
+
+        public SignalREventConsumer(string HubURL , ILogger logger)
+        {
+            _logger = logger;
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(HubURL)
+                .Build();
+        }
+
+        public async Task StartAsync()
+        {
+            try
+            {
+                await _hubConnection.StartAsync();
+                _logger.LogInformation($"Connected to SignalR hub at {_hubConnection.ConnectionId}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error connecting to SignalR hub: {ex.Message}");
+            }
+        }
+
+        public async Task StopAsync()
+        {
+            await _hubConnection.StopAsync();
+            _logger.LogInformation("SignalR connection stopped.");
+        }
+
+        public void SubscribeToEvents()
+        {
+            _hubConnection.On<string>("ReceiveAnomalyAlertAsync", (message) =>
+            {
+                _logger.LogInformation($"Received Anomaly Alert: {message}");
+            });
+
+            _hubConnection.On<string>("ReceiveHighUsageAlertAsync", (message) =>
+            {
+                _logger.LogInformation($"Received High Usage Alert: {message}");
+            });
+        }
+
+        public void Dispose()
+        {
+            _hubConnection.DisposeAsync();
+        }
+    }
+
+}
