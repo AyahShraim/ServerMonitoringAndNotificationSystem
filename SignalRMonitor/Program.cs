50629c3b6a0806c106d683f3cd96c8cfcfc10138
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-21 02:24:24 +0200

    add end point for alert hub

--- SignalRMonitor/Program.cs
+++ SignalRMonitor/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using SignalRMonitor;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+
+builder.Services.AddSignalR();
+builder.Services.AddScoped<SignalRAlertHub>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<SignalRAlertHub>("/alerthub");
+    endpoints.MapGet("/", context => context.Response.WriteAsync("Hello World!"));
+});
+
+var configuration = builder.Configuration;
+var signalRConsumer = SignalREventConsumer.Create(configuration);
+signalRConsumer.SubscribeToEvents();
+
 
 app.Run();
