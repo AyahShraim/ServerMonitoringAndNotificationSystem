6d6168f79a999218ae1c5afe4615b3b515d75f5d
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-23 00:09:16 +0200

    update alerthub to use Ilogger

--- SignalRMonitor/Hubs/AlertHub.cs
+++ SignalRMonitor/Hubs/AlertHub.cs
@@ -5,13 +5,21 @@ namespace SignalRMonitor.Hubs
 {
     public sealed class SignalRAlertHub : Hub<IAlertService>
     {
+        private readonly ILogger<SignalRAlertHub> _logger;
+        public SignalRAlertHub(ILogger<SignalRAlertHub> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task SendAnomalyAlertAsync(string message)
         {
+            _logger.LogInformation($"Alert Hub Received Anomaly Alert: {message}");
             await Clients.All.ReceiveAnomalyAlertAsync($"{Context.ConnectionId} : {message} ");
         }
 
         public async Task SendHighUsageAlertAsync(string message)
         {
+            _logger.LogInformation($"Alert Hub Received High usage Alert: {message}");
             await Clients.All.ReceiveHighUsageAlertAsync($"{Context.ConnectionId} : {message} ");
         }
     }
