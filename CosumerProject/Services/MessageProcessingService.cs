1bb789fad2dcd5268d603d20ed48c5ed81dfa379
Ayah Shraim <ayahshraim558@gmail.com>
2024-01-23 00:08:13 +0200

    update msg processing and consume service to use the class library project

---
+++ CosumerProject/Services/MessageProcessingService.cs
@@ -0,0 +1,30 @@
+﻿using ConsumerProject.Interfaces;
+using ConsumerProject.models;
+
+namespace ConsumerProject.Services
+{
+    public class MessageProcessingService
+    {
+        private readonly IMessageQueueClientConsumer _messageConsumer;
+        private readonly IDbService _mongoDbService;
+        private readonly AnomalyDetectionService _anomalyDetectionService;
+
+        public MessageProcessingService(IMessageQueueClientConsumer messageConsumer, IDbService mongoDbService, AnomalyDetectionService anomalyDetectionService)
+        {
+            _messageConsumer = messageConsumer;
+            _anomalyDetectionService = anomalyDetectionService;
+            _mongoDbService = mongoDbService;
+        }
+
+        public async Task StartProcessingAsync(string topicToConsumeFrom)
+        {
+            await _messageConsumer.ConsumeAsync<ServerStatistics>(topicToConsumeFrom, ProcessServerStatistics);
+        }
+
+        private void ProcessServerStatistics(ServerStatistics serverStatistics)
+        {
+             _mongoDbService.InsertOneAsync(serverStatistics).Wait(); 
+             _anomalyDetectionService.CheckForAnomalies(serverStatistics).Wait();
+        }
+    }
+}
